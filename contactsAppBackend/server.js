3d73b7230cac4af949422a5697caf6a1e4f4e412
rithvik23-root <rithvikarelli7@gmail.com>
2025-01-16 13:09:50 -0600

    implemented GET by name or email

--- contactsAppBackend/server.js
+++ contactsAppBackend/server.js
@@ -17,22 +17,39 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
-app.post('/contacts', async(req,res)=>{
-    try{
-        const{name,email}=req.body;
-        if(!email||!name){
-            return res.status(400).json({message:'Email and Name are required'});
-        }
-        const contact=new Contact({name,email});
-        await contact.save();
-        res.status(201).json(contact);
-    }catch(err){
-        res.status(500).json({error:err.message});
+app.post("/contacts", async (req, res) => {
+  try {
+    const { name, email } = req.body;
+    if (!email || !name) {
+      return res.status(400).json({ message: "Email and Name are required" });
     }
-})
+    const contact = new Contact({ name, email });
+    await contact.save();
+    res.status(201).json(contact);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
+app.get("/contacts", async (req, res) => {
+  try {
+    const query = req.query.query;
+    let filter = {};
 
-
+    if (query) {
+      filter = {
+        $or: [
+          { name: { $regex: new RegExp(query, 'i') } },
+          { email: { $regex: new RegExp(query, 'i') } }
+        ]
+      };
+    }
+    const contacts = await Contact.find(filter);
+    res.json(contacts);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
